6ea93267f1e9bc3a7d44d74f0213f4ad23c312da
AlainaAK <97328364+AlainaAK@users.noreply.github.com>
2022-05-19 03:43:00 -0700

    Beginning coroutine getting there

--- Assets/_Project/Scripts/Coroutines/ShadowFather.cs
+++ Assets/_Project/Scripts/Coroutines/ShadowFather.cs
@@ -9,23 +9,31 @@ namespace ACoolTeam
         [SerializeField] private GameObject _fatherObject;
         [SerializeField] private ConversationObject _dialogue1;
         [SerializeField] private ConversationObject _dialogue2;
+
+        private float _animWaitOne = 2f;
+        private float _animWaitTwo = 4f;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             //if (collision.tag == "Player")
 
-
+            StartCoroutine(FatherAppear());
 
         }
 
         private IEnumerator FatherAppear()
         {
-            ConversationManager.Instance.StartConversation(_dialogue1, false, gameObject);
+            _fatherObject.GetComponent<Animator>().SetInteger("Stage", 1);
+            yield return new WaitForSeconds(_animWaitOne);
             _fatherObject.GetComponent<Animator>().SetInteger("Stage", 2);
+            ConversationManager.Instance.StartConversation(_dialogue1, false, gameObject);
             while (ConversationManager.Instance.IsTalking() == true)
             {
-                yield return null;
+                Debug.Log(ConversationManager.Instance.IsTalking());
+                yield return new WaitForSeconds(0.1f);
             }
             _fatherObject.GetComponent<Animator>().SetInteger("Stage", 3);
+            yield return new WaitForSeconds(_animWaitTwo);
             _fatherObject.GetComponent<Animator>().SetInteger("Stage", 4);
             ConversationManager.Instance.StartConversation(_dialogue2, false, gameObject);
         }
