1f49c24bcebb36ad8b69a1a08f8ac8d286658d67
Jalagon13 <A1ag0nJus1in@gmail.com>
2022-05-18 22:29:01 -0700

    Sweet Spot now  moves after each completed hit

--- Assets/_Project/Scripts/Systems/Puzzle System/Ritual Puzzle/SweetSpot.cs
+++ Assets/_Project/Scripts/Systems/Puzzle System/Ritual Puzzle/SweetSpot.cs
@@ -8,6 +8,12 @@ namespace ACoolTeam
     public class SweetSpot : MonoBehaviour
     {
         private bool _onLineIndicator;
+        private RectTransform _sweetSpotTransform;
+
+        private void Awake()
+        {
+            _sweetSpotTransform = GetComponent<RectTransform>();
+        }
 
         private void OnEnable()
         {
@@ -24,6 +30,7 @@ namespace ACoolTeam
             if (_onLineIndicator)
             {
                 Debug.Log("Moving Sweet Spot...");
+                _sweetSpotTransform.anchoredPosition = new Vector3(UnityEngine.Random.Range(0.0f, 360.0f), 0.0f, 0.0f);
             }
         }
 
