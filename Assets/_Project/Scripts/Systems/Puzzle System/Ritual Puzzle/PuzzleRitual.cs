5819d8945220521ac32388ae8ae2c57e9135f10c
Jalagon13 <A1ag0nJus1in@gmail.com>
2022-05-30 12:18:00 -0700

    Jumpscare spooky af

--- Assets/_Project/Scripts/Systems/Puzzle System/Ritual Puzzle/PuzzleRitual.cs
+++ Assets/_Project/Scripts/Systems/Puzzle System/Ritual Puzzle/PuzzleRitual.cs
@@ -13,11 +13,16 @@ namespace ACoolTeam
         public static event PauseIndicatorAction OnPauseIndicator;
         public static int GameProgress = 3;
 
-        [SerializeField] private Image _blackBackground;
         [SerializeField] private Animator _lineIndicatorAnimator;
         [SerializeField] private AnimationClip _indicatorAnimation;
         [SerializeField] private BoxCollider2D _indicatorCollider;
         [SerializeField] private BoxCollider2D _sweetSpotCollider;
+        [Space(10)]
+        [SerializeField] private Image _blackBackground;
+        [SerializeField] private RawImage jumpscare1;
+        [SerializeField] private RawImage jumpscare2;
+        [SerializeField] private RawImage jumpscare3;
+
 
         private PlayerInput _playerInput;
         private bool _canInput = true;
@@ -77,7 +82,15 @@ namespace ACoolTeam
         private IEnumerator OnLevelComplete()
         {
             _blackBackground.enabled = true;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1.5f);
+            GetComponent<SM_PlaySound>().PlayClip(SM_PlaySound.SoundType.SFX);
+            yield return new WaitForSeconds(.5f);
+            jumpscare1.enabled = true;
+            yield return new WaitForSeconds(.15f);
+            jumpscare2.enabled = true;
+            yield return new WaitForSeconds(.15f);
+            jumpscare3.enabled = true;
+            yield return new WaitForSeconds(2.5f);
             SceneManager.LoadScene("MenuScene");
         }
 
